1c24bd1f6a7aa152c5277655affb05504ce6f63a
Cris Corrêa <ecmac@cin.ufpe.br>
2018-11-25 20:47:09 -0300

    Tela de apresentacao ajustada, com imagem

--- app/src/main/java/br/com/serttel/semaforosrecife/SplashActivity.java
+++ app/src/main/java/br/com/serttel/semaforosrecife/SplashActivity.java
@@ -1,11 +1,9 @@
 package br.com.serttel.semaforosrecife;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends Activity {
