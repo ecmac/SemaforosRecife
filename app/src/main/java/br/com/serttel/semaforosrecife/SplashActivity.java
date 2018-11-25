c923647653e0c74474b09134934a2263bc2f0f8e
Cris Corrêa <ecmac@cin.ufpe.br>
2018-11-25 15:34:09 -0300

    Tela de apresentacao

---
+++ app/src/main/java/br/com/serttel/semaforosrecife/SplashActivity.java
@@ -0,0 +1,33 @@
+package br.com.serttel.semaforosrecife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this,MapsActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
