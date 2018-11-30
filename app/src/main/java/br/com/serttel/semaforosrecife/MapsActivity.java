800ef1cb644caa693decdf75f7048a593781de71
Eladia Cristina Motta de Assis Correa <ecmac@WINDOWS.CIN.UFPE.BR>
2018-11-30 12:45:20 -0300

    Removida leitura de arquivo salvo e adicionada tentativa de requisicao

--- app/src/main/java/br/com/serttel/semaforosrecife/MapsActivity.java
+++ app/src/main/java/br/com/serttel/semaforosrecife/MapsActivity.java
@@ -1,6 +1,7 @@
 package br.com.serttel.semaforosrecife;
 
 import android.app.AlertDialog;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,14 +11,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.util.JsonReader;
-import android.util.JsonToken;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerClickListener,
         OnMapReadyCallback {
@@ -26,6 +19,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
     private Semaforo[] semaforos;
     String linkStart, linkNext;
     int limit, total;
+    boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,33 +28,31 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+        mapFragment.getMapAsync(this); }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
 
-        semaforos = new Semaforo[50];
+        NetworkThread networkThread = new NetworkThread(mMap, linkStart, linkNext, limit, total);
+        networkThread.start();
         try {
-            BufferedReader fileReader = new BufferedReader(new InputStreamReader(getAssets().open("dadosRecifeSemaforo.json")));
-            JsonReader jsonReader = new JsonReader(fileReader); //classe que reconhece arquivo como json
-            parseJSON(jsonReader);
-            jsonReader.close();
-        }
-        catch (IOException e) {
+            networkThread.join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        mMap = networkThread.getmMap();
+        semaforos = networkThread.getSemaforos();
+        linkStart = networkThread.getLinkStart();
+        linkNext = networkThread.getLinkNext();
+        limit = networkThread.getLimit();
+        total = networkThread.getTotal();
+
+        //semaforos = new Semaforo[50];
+
+        //jsonRequisition();
 
         //ponto mais central
         LatLng central = new LatLng(-8.082688,-34.906625);
@@ -70,106 +62,7 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
         mMap.setOnMarkerClickListener(this);
     }
 
-    public void parseJSON (JsonReader jsonReader){
 
-        String ut, loc1 =  null, loc2 = null, func = null, ss = null, sc = null;
-        int sem = 0, _id = 0, i = 0;
-        double lg = 0, lt = 0;
-
-        try {
-            jsonReader.beginObject();
-
-            while(!jsonReader.peek().equals(JsonToken.END_DOCUMENT)) {
-
-                if (jsonReader.peek().equals(JsonToken.NAME)) {
-                    String nextName = jsonReader.nextName();
-
-                    if(nextName.equals("utilizacao")) {
-                        /*"records" eh o nome do array de semaforos.
-                         * nao sendo ele, ja eh o primeiro atributo de um semaforo
-                         */
-
-                        ut = jsonReader.nextString();
-
-                        boolean loop = true;
-                        while(loop){
-
-                            String nextNameObj = jsonReader.nextName();
-
-                            if (nextNameObj.equals("localizacao1")){
-                                loc1 = jsonReader.nextString();
-                            }
-                            else if (nextNameObj.equals("localizacao2")){
-                                loc2 = jsonReader.nextString();
-                            }
-                            else if (nextNameObj.equals("funcionamento")){
-                                func = jsonReader.nextString();
-                            }
-                            else if (nextNameObj.equals("sinalsonoro")){
-                                ss = jsonReader.nextString();
-                            }
-                            else if (nextNameObj.equals("semaforo")){
-                                sem = jsonReader.nextInt();
-                            }
-                            else if (nextNameObj.equals("sinalizadorciclista")){
-                                sc = jsonReader.nextString();
-                            }
-                            else if (nextNameObj.equals("Longitude")){
-                                lg = jsonReader.nextDouble();
-                            }
-                            else if (nextNameObj.equals("Latitude")){
-                                lt = jsonReader.nextDouble();
-                            }
-                            else if (nextNameObj.equals("_id")){
-                                _id = jsonReader.nextInt();
-                                loop = false;
-                                semaforos[i] = new Semaforo(ut, loc1, loc2, func, ss, sem, sc, lg, lt, _id); //cria novo semaforo e adiciona ao array
-
-                                LatLng pos = new LatLng(lt, lg);
-
-                                Marker marker = mMap.addMarker(new MarkerOptions().position(pos).title("Semaforo #" + sem));
-                                marker.setTag(i);
-
-                                i++;
-                            }
-                        }
-                    }
-                    else if(nextName.equals("_links")){
-                        jsonReader.beginObject();
-                        jsonReader.nextName();
-                        linkStart = jsonReader.nextString();
-                        jsonReader.nextName();
-                        linkNext = jsonReader.nextString();
-                        jsonReader.endObject();
-                    }
-                    else if(nextName.equals("limit")){
-                        limit = jsonReader.nextInt();
-                    }
-                    else if(nextName.equals("total")){
-                        total = jsonReader.nextInt();
-                    }
-
-                }
-                else if (jsonReader.peek().equals(JsonToken.BEGIN_ARRAY)) {
-                    jsonReader.beginArray();
-                }
-                else if (jsonReader.peek().equals(JsonToken.BEGIN_OBJECT)){
-                    jsonReader.beginObject();
-                }
-                else if (jsonReader.peek().equals(JsonToken.END_OBJECT)) {
-                    jsonReader.endObject();
-                }
-                else if (jsonReader.peek().equals(JsonToken.END_ARRAY)) {
-                    jsonReader.endArray();
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
 
     @Override
     public boolean onMarkerClick(final Marker marker){
@@ -184,4 +77,5 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
 
         return false;
     }
+
 }
---
+++ app/src/main/java/br/com/serttel/semaforosrecife/NetworkThread.java
@@ -0,0 +1,197 @@
+package br.com.serttel.semaforosrecife;
+
+import android.os.AsyncTask;
+import android.util.JsonReader;
+import android.util.JsonToken;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkThread extends Thread {
+
+    private GoogleMap mMap;
+    private Semaforo[] semaforos;
+    private String linkStart;
+    private String linkNext;
+    private int limit, total;
+
+    public NetworkThread(GoogleMap mMap,String linkStart, String linkNext, int limit, int total){
+        this.mMap = mMap;
+        this.semaforos = new Semaforo[50];
+        this.linkStart = linkStart;
+        this.linkNext = linkNext;
+        this.limit = limit;
+        this.total = total;
+    }
+
+    public Semaforo[] getSemaforos() {
+        return semaforos;
+    }
+
+    public GoogleMap getmMap() {
+
+        return mMap;
+    }
+
+    public String getLinkNext() {
+        return linkNext;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getLimit() {
+
+        return limit;
+    }
+
+    public String getLinkStart() {
+
+        return linkStart;
+    }
+
+    private void jsonRequisition() {
+
+        HttpURLConnection httpConnection = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+            URL url = new URL("http://desafio.serttel.com.br/dadosRecifeSemaforo.json");
+            httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.connect(); //conexao http para acessar o arquivo
+
+            InputStream inputStream = httpConnection.getInputStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream)); //obter o arquivo a partir da conexao
+
+            JsonReader jsonReader = new JsonReader(bufferedReader); //classe que reconhece arquivo como json
+            parseJSON(jsonReader);
+            jsonReader.close();
+
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseJSON (JsonReader jsonReader){
+
+        String ut, loc1 =  null, loc2 = null, func = null, ss = null, sc = null;
+        int sem = 0, _id = 0, i = 0;
+        double lg = 0, lt = 0;
+
+        try {
+            jsonReader.beginObject();
+
+            while(!jsonReader.peek().equals(JsonToken.END_DOCUMENT)) {
+
+                if (jsonReader.peek().equals(JsonToken.NAME)) {
+                    String nextName = jsonReader.nextName();
+
+                    if(nextName.equals("utilizacao")) {
+                        /*"records" eh o nome do array de semaforos.
+                         * nao sendo ele, ja eh o primeiro atributo de um semaforo
+                         */
+
+                        ut = jsonReader.nextString();
+
+                        boolean loop = true;
+                        while(loop){
+
+                            String nextNameObj = jsonReader.nextName();
+
+                            if (nextNameObj.equals("localizacao1")){
+                                loc1 = jsonReader.nextString();
+                            }
+                            else if (nextNameObj.equals("localizacao2")){
+                                loc2 = jsonReader.nextString();
+                            }
+                            else if (nextNameObj.equals("funcionamento")){
+                                func = jsonReader.nextString();
+                            }
+                            else if (nextNameObj.equals("sinalsonoro")){
+                                ss = jsonReader.nextString();
+                            }
+                            else if (nextNameObj.equals("semaforo")){
+                                sem = jsonReader.nextInt();
+                            }
+                            else if (nextNameObj.equals("sinalizadorciclista")){
+                                sc = jsonReader.nextString();
+                            }
+                            else if (nextNameObj.equals("Longitude")){
+                                lg = jsonReader.nextDouble();
+                            }
+                            else if (nextNameObj.equals("Latitude")){
+                                lt = jsonReader.nextDouble();
+                            }
+                            else if (nextNameObj.equals("_id")){
+                                _id = jsonReader.nextInt();
+                                loop = false;
+                                semaforos[i] = new Semaforo(ut, loc1, loc2, func, ss, sem, sc, lg, lt, _id); //cria novo semaforo e adiciona ao array
+
+                                LatLng pos = new LatLng(lt, lg);
+
+                                Marker marker = mMap.addMarker(new MarkerOptions().position(pos).title("Semaforo #" + sem));
+                                marker.setTag(i);
+
+                                i++;
+                            }
+                        }
+                    }
+                    else if(nextName.equals("_links")){
+                        jsonReader.beginObject();
+                        jsonReader.nextName();
+                        linkStart = jsonReader.nextString();
+                        jsonReader.nextName();
+                        linkNext = jsonReader.nextString();
+                        jsonReader.endObject();
+                    }
+                    else if(nextName.equals("limit")){
+                        limit = jsonReader.nextInt();
+                    }
+                    else if(nextName.equals("total")){
+                        total = jsonReader.nextInt();
+                    }
+
+                }
+                else if (jsonReader.peek().equals(JsonToken.BEGIN_ARRAY)) {
+                    jsonReader.beginArray();
+                }
+                else if (jsonReader.peek().equals(JsonToken.BEGIN_OBJECT)){
+                    jsonReader.beginObject();
+                }
+                else if (jsonReader.peek().equals(JsonToken.END_OBJECT)) {
+                    jsonReader.endObject();
+                }
+                else if (jsonReader.peek().equals(JsonToken.END_ARRAY)) {
+                    jsonReader.endArray();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void run() {
+
+        jsonRequisition();
+
+    }
+}
